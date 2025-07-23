ae087c06f3c3d29a6ab1af1e71c2a8d59fd4f7d1
yusufziy <82224276+yusufziy@users.noreply.github.com>
2025-07-23 23:19:52 +0300

    Update index.js

--- index.js
+++ index.js
@@ -2,20 +2,18 @@ const express = require('express');
 const app = express();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
+const path = require('path');
+
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static(__dirname));
+app.use(express.static(__dirname)); // Statik dosya servisi
 
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html');
+    res.sendFile(path.join(__dirname, 'index.html')); // daha stabil
 });
 
 io.on('connection', (socket) => {
     console.log('Bir kullanıcı bağlandı');
-
-    socket.on('chat message', (msg) => {
-        io.emit('chat message', msg);
-    });
 
     socket.on('disconnect', () => {
         console.log('Kullanıcı ayrıldı');
